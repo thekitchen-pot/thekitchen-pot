24e95dd43a6bc3f2f6544825e4686903d66044ae
Patrick Mahinge <patrick@mahinge.com>
2025-06-11 12:46:45 +0000

    Header

---
+++ src/components/navigation/config.ts
@@ -0,0 +1,8 @@
+// Move links array outside the component
+export const navigationLinks = Object.freeze([
+  { href: "/", text: "Home" },
+  { href: "/blog/", text: "Blog" },
+  { href: "/about/", text: "About" },
+  { href: "/blog/category/how-to-guides/", text: "Resources" },
+  { href: "/contact-us/", text: "Contact" },
+]);
